55c562af291a3ecd7c8bda43fda13d75deaaa345
casteem <41547936+casteem@users.noreply.github.com>
2018-08-03 03:14:05 +0800

    Update tag-storage.js

--- js/tag-storage.js
+++ js/tag-storage.js
@@ -1,6 +1,6 @@
 window.TagStorage = (function() {
   var KEY = 'TAG';
-  var tags = [ 'kr', 'dev' ];
+  var tags = [ 'utopian-io', 'curable' ];
 
   var save = function() {
     localStorage.setItem(KEY, tags.join(','), 3650);
